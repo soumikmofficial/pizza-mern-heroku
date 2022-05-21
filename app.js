f990abb184ee2f3c92647b8e7c06ed75c4bd70c2
soumikm.official@gmail.com <soumikmofficial@gmail.com>
2022-05-21 05:43:45 +0530

    helmet setting modified

--- app.js
+++ app.js
@@ -33,12 +33,8 @@ app.use(cors());
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
-      "default-src": ["'self'"],
-      "connect-src": ["'self'", "'unsafe-inline'"],
       "img-src": ["'self'", "data:"],
-      "style-src-elem": ["'self'", "data:"],
       "script-src": ["'unsafe-inline'", "'self'"],
-      "object-src": ["'none'"],
     },
   })
 );
